91d701e892dae951d4c4cb17df233edad6a7d636
Nikita Shinde <nikita.shinde3@outlook.com>
2024-07-04 13:14:20 +0530

    Added different types of left side triangle

--- src/LeftSideTriangle.java
+++ src/LeftSideTriangle.java
@@ -19,5 +19,78 @@ public class LeftSideTriangle {
             }
             System.out.println();
         }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        System.out.println("Number Upright triangle:");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        System.out.println("Number inverted Triangle");
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        int number=10;
+
+        System.out.println("Increamental number upright triangle:");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        System.out.println("Increamental numbers inverted triangle:");
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        System.out.println("Binary number upright triangle:");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(j%2==0){
+                    System.out.print(0);
+                }else{
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        System.out.println("Binary numbers inverted triangle");
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                if(j%2==0){
+                    System.out.print(0);
+                }else{
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
