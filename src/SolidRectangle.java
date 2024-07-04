f99312be27ea314db4d5dab9cdf47d3ebdbabada
Nikita Shinde <nikita.shinde3@outlook.com>
2024-07-04 12:25:15 +0530

    Added Solid REctangle

---
+++ src/SolidRectangle.java
@@ -0,0 +1,12 @@
+public class SolidRectangle {
+    public static void main(String[] args) {
+        int lenght = 4;
+        int height = 5;
+        for(int i=1;i<=lenght;i++){
+            for(int j=1;j<=height;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
