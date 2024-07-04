67a197ec7b9d50c7ddbae395c51f0957c4865814
Nikita Shinde <nikita.shinde3@outlook.com>
2024-07-04 12:43:06 +0530

    Added triangle

---
+++ src/LeftTriangle.java
@@ -0,0 +1,23 @@
+public class LeftTriangle {
+    public static void main(String[] args) {
+        int n=5;
+
+        System.out.println("Upright triangle:");
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        System.out.println("Inverted triangle:");
+        for(int i =n;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
